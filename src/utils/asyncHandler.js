5cc1644c3288cc798e651e54f11181a55d865a08
SauravChandra10 <sauravchandra10@gmail.com>
2024-01-02 23:48:52 +0530

    Utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) =>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler};
+
+/*
+const asyncHandler = ()=>{}
+if we want to pass a function func
+const asyncHandler = (func) => {()=>{}} or asyncHandler = () => () => {}
+
+try catch implementation: 
+
+const asyncHandler = (fn) => async(req,res,next) =>{
+    try{
+        await fn(req,res,next)
+    } catch(error){
+        res.send(err.code || 500).json({
+            success:false,
+            message:err.message 
+        })
+    }
+}
+*/
